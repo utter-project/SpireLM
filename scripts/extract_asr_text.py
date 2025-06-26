6667fad1ba15d81c343d61ed1b4830307b9a9111
Ben Peters <bpeters@coli.uni-saarland.de>
2025-06-26 14:01:13 +0000

    [wip] refactor datasets for labeling

--- scripts/extract_asr_text.py
+++ scripts/extract_asr_text.py
@@ -2,9 +2,17 @@ import argparse
 import json
 from datasets import load_dataset
 
+from spire.data import load_hf_audio_dataset
+
 
 def main(args):
-    dataset = load_dataset(args.path, args.path_extra, split=args.split, trust_remote_code=True)
+    # we don't want this
+    # dataset = load_dataset(args.path, args.path_extra, split=args.split, trust_remote_code=True)
+
+    # add from_disk)
+    dataset = load_hf_audio_dataset(
+        args.path, path_extra=args.path_extra, split=args.split, from_disk=
+    ).remove_columns("audio")
 
     text = dataset[args.text_field]
     with open(args.corpus, "w") as corpus_f:
@@ -22,9 +30,11 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # parser.add_argument("--dataset", choices=["ls-clean", "ls-other", "voxpopuli", "fleurs"])
-    parser.add_argument("--path", choices=["google/fleurs", "openslr/librispeech_asr", "facebook/voxpopuli"])
-    parser.add_argument("--path-extra")  # generally a language or clean/other
+    # parser.add_argument("--path", choices=["google/fleurs", "openslr/librispeech_asr", "facebook/voxpopuli"])
+    parser.add_argument("--path", required=True)
+    parser.add_argument("--path-extra", default="")  # generally a language or clean/other
     parser.add_argument("--split", default="test")
+    parser.add_argument("--dataset-type", default="tsv", choices=["tsv", "hf-disk", "hf-cache"])
     parser.add_argument("--text-field", required=True)
     parser.add_argument("--corpus", required=True)
     parser.add_argument("--metadata", required=True)
