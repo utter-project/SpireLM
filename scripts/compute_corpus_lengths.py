a0fa3d4895d6a59661279852a96b19f6cca71474
Ben Peters <bpeters@coli.uni-saarland.de>
2025-07-16 10:24:33 +0100

    add script for computing example lengths and saving as npy

---
+++ scripts/compute_corpus_lengths.py
@@ -0,0 +1,55 @@
+import argparse
+import numpy as np
+
+from datasets import disable_caching
+
+from spire.data import load_hf_audio_dataset
+
+
+def compute_commonvoice_time(example):
+    example["seconds"] = example["audio"]["array"].shape[0] / example["audio"]["sample_rate"]
+    return example
+
+
+def compute_spgi_time(example):
+    example["seconds"] = example["wav_filesize"] / 32000  # I believe this is correct
+    return example
+
+
+def compute_gigaspeech_time(example):
+    example["seconds"] = example["end_time"] - example["begin_time"]
+    return example
+
+
+def main(args):
+    compute_time_funcs = {
+        "commonvoice": compute_commonvoice_time,
+        "spgi": compute_spgi_time,
+        "gigaspeech": compute_gigaspeech_time
+    }
+    compute_time_func = compute_time_funcs[args.corpus_type]
+
+    dataset = load_hf_audio_dataset(
+        args.path, path_extra=args.path_extra, split=args.split
+    )
+    if args.corpus_type != "commonvoice":
+        # For SPGI and Gigaspeech, lengths can be computed without loading the
+        # audio files
+        dataset = dataset.remove_columns("audio")
+
+    disable_caching()
+    dataset = dataset.map(compute_time_func)
+
+    times = np.array(dataset["seconds"])
+    np.save(args.out, times)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--corpus-type", choices=["commonvoice", "spgi", "gigaspeech"])
+    parser.add_argument("--path")
+    parser.add_argument("--path-extra", nargs="?", const="")
+    parser.add_argument("--split", default="train")
+    parser.add_argument("--out", default="times.npy")
+    args = parser.parse_args()
+    main(args)
