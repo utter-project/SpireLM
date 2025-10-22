a061aac6788a1072993095dfdac307aaa874b3a6
Ben Peters <bpeters@coli.uni-saarland.de>
2025-10-22 17:25:50 +0100

    add new requirements to setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,16 @@
 
 from setuptools import setup
 
+# many of these can probably be relaxed
+requirements = [
+    "torch>=2.8.0",
+    "transformers>=4.57.1",
+    "datasets>=2.19.2,<4.0.0",
+    "vllm>=0.11.0",
+    "protobuf>=5.29.3",
+    "scikit-learn>=1.3.2"
+]
+
 setup(name='SpireLM',
       version='0.1',
       description='LLM for speech and text tasks',
@@ -9,5 +19,6 @@ setup(name='SpireLM',
       author_email='benzurdopeters@gmail.com',
       url='nunya',
       packages=['spire'],
+      install_requires=requirements,
       license="MIT"
      )
