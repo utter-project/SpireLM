304e6b59d44c076188d49a03ccedebe9eae3db4b
Ben Peters <bpeters@coli.uni-saarland.de>
2026-01-07 16:58:37 +0000

    remove incorrect args

--- spire/utils.py
+++ spire/utils.py
@@ -105,6 +105,6 @@ def load_features(feat_files=None, feat_dir=None):
     assert feat_dir is None or feat_files is None
 
     if feat_dir is not None:
-        feat_files = glob(join(args.feat_dir, "*.npy"))
+        feat_files = glob(join(feat_dir, "*.npy"))
 
     return np.vstack([np.load(p) for p in feat_files])
