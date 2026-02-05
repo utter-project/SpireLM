c95abe6b8367032ca272eeb6a808f9feaa43e68d
Ben Peters <bpeters@coli.uni-saarland.de>
2026-02-05 18:07:11 +0000

    remove path-fixing functions (unnecessary now that code supports loading directly from HF datasets)

--- spire/utils.py
+++ spire/utils.py
@@ -67,39 +67,6 @@ def detokenize(labels, indices_only=False, deduplicated=True):
     return labels
 
 
-def fix_librispeech_path(ex_dict, path_extra, split):
-    """
-    This seems to work
-    """
-    alleged_path = ex_dict["file"]
-    if exists(alleged_path):
-        return alleged_path
-
-    dir_part = dirname(alleged_path)
-
-    path_split = "dev" if split == "validation" else split
-
-    new_path = join(
-        dir_part,
-        "LibriSpeech", "-".join([path_split, path_extra]),
-        str(ex_dict["speaker_id"]),
-        str(ex_dict["chapter_id"]),
-        str(ex_dict["id"]) + ".flac"
-    )
-
-    return new_path
-
-
-def fix_fleurs_path(ex_dict, split):
-    alleged_path = ex_dict["path"]
-    if exists(alleged_path):
-        return alleged_path
-
-    path_split = "dev" if split == "validation" else split
-
-    return join(dirname(alleged_path), path_split, basename(alleged_path))
-
-
 def load_features(feat_files=None, feat_dir=None, n_files=0):
     assert feat_dir is not None or feat_files is not None
     assert feat_dir is None or feat_files is None
