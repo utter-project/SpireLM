0b64eb35c5b00535245ffd55cb1dc552a67e2ea3
Ben Peters <bpeters@coli.uni-saarland.de>
2026-04-17 12:15:05 +0100

    remove unnecessary MiniBatchKMeans arguments

--- scripts/features-and-kmeans.py
+++ scripts/features-and-kmeans.py
@@ -34,12 +34,9 @@ def main(args):
     kmeans = MiniBatchKMeans(
         n_clusters=args.n_clusters,
         init=args.init,
-        max_iter=args.max_iter,
         batch_size=args.kmeans_batch_size,
         verbose=args.verbose,
         compute_labels=False,
-        tol=args.tol,
-        max_no_improvement=args.max_no_improvement,
         init_size=None,
         n_init=args.n_init if args.n_init > 0 else "auto",
         reassignment_ratio=args.reassignment_ratio,
@@ -121,8 +118,7 @@ def main(args):
                     batch_hours = sum(seconds_buffer[:args.kmeans_batch_size]) / 3600
                     seconds_buffer = []
 
-                    # update hours seen
-                    n_hours += batch_hours
+                    n_hours += batch_hours  # update hours seen
 
                     pbar.update(batch_hours)
 
@@ -160,13 +156,9 @@ if __name__ == "__main__":
     parser.add_argument("--save-intermediate", action="store_true")
     parser.add_argument("--n-clusters", type=int, default=5000)
     parser.add_argument("--init", default="k-means++", choices=["k-means++", "random"])
-    parser.add_argument("--max_iter", default=100, type=int)
-    parser.add_argument("--tol", default=0.0, type=float)
-    parser.add_argument("--max_no_improvement", default=100, type=int)
-    parser.add_argument("--n_init", default=0, type=int)
-    parser.add_argument("--reassignment_ratio", default=0.01, type=float)  # resetting default to avoid dead centers
+    parser.add_argument("--n-init", default=0, type=int)
+    parser.add_argument("--reassignment-ratio", default=0.01, type=float)  # resetting default to avoid dead centers
     parser.add_argument("--verbose", type=int, default=1)
-    # everything below: minibatch-specific arguments
     parser.add_argument("--validation-steps", type=int, default=100)
     parser.add_argument("--centroid-delta", type=float, default=1e-4)
     args = parser.parse_args()
